0749e4a5beb8e0f459d0800b4c6c7984eec94c6a
wolnyludz <urbi90@gmail.com>
2017-12-17 15:41:51 +0100

    gulpfile.js - concat gsap too

--- gulpfile.js
+++ gulpfile.js
@@ -1,71 +1,75 @@
 /*global require*/
-const del = require("del");
-const gulp = require("gulp");
-const autoprefixer = require("gulp-autoprefixer");
-const browsersync = require("browser-sync").create();
-const cache = require("gulp-cache");
-const concat = require("gulp-concat");
-const cssnano = require("gulp-cssnano");
-const imagemin = require("gulp-imagemin");
-const notify = require("gulp-notify");
-const rename = require("gulp-rename");
-const sass = require("gulp-sass");
-const uglify = require("gulp-uglify-es").default;
+/* eslint-disable no-console */
+const del = require(`del`);
+const gulp = require(`gulp`);
+const autoprefixer = require(`gulp-autoprefixer`);
+const browsersync = require(`browser-sync`).create();
+const cache = require(`gulp-cache`);
+const concat = require(`gulp-concat`);
+const cssnano = require(`gulp-cssnano`);
+const imagemin = require(`gulp-imagemin`);
+const notify = require(`gulp-notify`);
+const plumber = require(`gulp-plumber`);
+const rename = require(`gulp-rename`);
+const sass = require(`gulp-sass`);
+const uglify = require(`gulp-uglify-es`).default;
 
-
-gulp.task("browsersync", function() {
+gulp.task(`browsersync`, function() {
   browsersync.init({
     server: {
-      baseDir: "./"
+      baseDir: `./`
     }
   });
 });
 
-gulp.task("styles", function () {
-  return gulp.src("./scss/*.scss")
+gulp.task(`styles`, function () {
+  return gulp.src(`./scss/*.scss`)
+    .pipe(plumber())
     .pipe(sass())
-    .pipe(autoprefixer("last 2 versions"))
-    .pipe(gulp.dest("./dist/"))
+    .pipe(autoprefixer(`last 2 versions`))
+    .pipe(gulp.dest(`./dist/`))
     .pipe(cssnano())
-    .pipe(rename({suffix: ".min"}))
-    .pipe(gulp.dest("./dist/"))
-    .pipe(notify({message: "Styles task complete"}))
+    .pipe(rename({suffix: `.min`}))
+    .pipe(gulp.dest(`./dist/`))
+    .pipe(notify({message: `styles task completed`}))
     .pipe(browsersync.reload({stream: true}));
 });
 
-gulp.task("scripts", function() {
-  return gulp.src("./js/*.js")
-    .pipe(concat("main.js"))
-    .pipe(gulp.dest("./dist/"))
-    .pipe(rename({ suffix: ".min" }))
+gulp.task(`scripts`, function() {
+  return gulp.src([`./node_modules/gsap/TweenLite.js`, `./js/*.js`])
+    .pipe(plumber())
+    .pipe(concat(`main.js`))
+    .pipe(gulp.dest(`./dist/`))
+    .pipe(rename({ suffix: `.min` }))
     .pipe(uglify())
-    .pipe(gulp.dest("./dist/"))
-    .pipe(notify({ message: "Scripts task complete" }));
+    .pipe(gulp.dest(`./dist/`))
+    .pipe(notify({ message: `scripts task completed` }));
 });
 
 
-gulp.task("images", function () {
-  return gulp.src("./src/images/**/*")
+gulp.task(`images`, function () {
+  return gulp.src(`./src/images/**/*`)
+    .pipe(plumber())
     .pipe(cache(imagemin({optimizationLevel: 3, progressive: true, interlaced: true})))
-    .pipe(gulp.dest("dist"))
-    .pipe(notify({message: "images task complete"}));
+    .pipe(gulp.dest(`dist`))
+    .pipe(notify({message: `images task completed`}));
 });
 
-gulp.task("clean", function () {
-  return del(["dist"]);
+gulp.task(`clean`, function () {
+  return del([`dist`]);
 });
 
 
-gulp.task("watch", ["browsersync"], function () {
+gulp.task(`watch`, [`browsersync`], function () {
 
 
-  gulp.watch("scss/*.scss", ["styles"]);
-  gulp.watch("js/*.js", ["scripts"]);
-  gulp.watch("images/*");
+  gulp.watch(`scss/*.scss`, [`styles`]);
+  gulp.watch(`js/*.js`, [`scripts`]);
+  gulp.watch(`images/*`);
 
-  gulp.watch("./*.html").on("change", browsersync.reload);
+  gulp.watch(`./*.html`).on(`change`, browsersync.reload);
 });
 
-gulp.task("default", ["clean"], function () {
-  gulp.start("styles", "scripts", "images");
+gulp.task(`default`, [`clean`], function () {
+  gulp.start(`styles`, `scripts`, `images`);
 });
