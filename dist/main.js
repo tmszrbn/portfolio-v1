ffad4138728cdbab09c0dd07bf5d349ecb12d435
wolnyludz <urbi90@gmail.com>
2017-12-18 02:30:58 +0100

    toggle navigation with maxHeight

--- dist/main.js
+++ dist/main.js
@@ -1,7 +1,28 @@
-let x = "lskjdflksdjf"
+/* global TweenMax */
 
-var a = 5324;
+const $button = document.querySelector(`button`);
+const $menu = document.querySelector(`ul`);
+const $menuIts = document.querySelectorAll(`li`);
 
-function functionName() {
+const getDark = (el, time) => {
+  TweenMax.to(el, time, {backgroundColor: `#000`, color: `#fff`});
+};
 
-}
+const getLight = (el, time) => {
+  TweenMax.to(el, time, {backgroundColor: `#fff`, color: `#000`});
+};
+
+
+
+$button.addEventListener(`mouseenter`, () => getDark($button, 0.3));
+
+$button.addEventListener(`mouseleave`, () => getLight($button, 0.3));
+
+$button.addEventListener(`click`, () => {
+  if ($menu.style.maxHeight > `0px`) {
+    TweenMax.to($menu, 0.5, {maxHeight: `0`});
+  }
+  else {
+    TweenMax.to($menu, 0.5, {maxHeight: `${window.getComputedStyle($menu.parentElement).height}`});
+  }
+});
--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
-var a = 5324;
+/* global TweenMax */
 
-function functionName() {
+const $button = document.querySelector(`button`);
+const $menu = document.querySelector(`ul`);
+const $menuIts = document.querySelectorAll(`li`);
 
-}
+const getDark = (el, time) => {
+  TweenMax.to(el, time, {backgroundColor: `#000`, color: `#fff`});
+};
+
+const getLight = (el, time) => {
+  TweenMax.to(el, time, {backgroundColor: `#fff`, color: `#000`});
+};
+
+
+
+$button.addEventListener(`mouseenter`, () => getDark($button, 0.3));
+
+$button.addEventListener(`mouseleave`, () => getLight($button, 0.3));
+
+$button.addEventListener(`click`, () => {
+  if ($menu.style.maxHeight > `0px`) {
+    TweenMax.to($menu, 0.5, {maxHeight: `0`});
+  }
+  else {
+    TweenMax.to($menu, 0.5, {maxHeight: `${window.getComputedStyle($menu.parentElement).height}`});
+  }
+});
