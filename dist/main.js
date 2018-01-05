b963b4fb07c06c61683a25e95b62c49cefb83dbc
wolnyludz <urbi90@gmail.com>
2018-01-05 10:29:08 +0100

    href to .links__item from anchor tag

--- dist/main.js
+++ dist/main.js
@@ -35,3 +35,19 @@ function scroll(toItem) {
 
   smoothScroll(targetY, pageH);
 }
+
+const navItems = Array.from(document.querySelectorAll(`[href^="#"]`));
+let scrollEvent;
+
+navItems.map(item => {
+  // add smoothScroll to onclick event to all navigation links
+  item.onclick = function (event) {
+    event.preventDefault();
+    const itemTarget = document.getElementById(item.getAttribute(`href`).slice(1));
+    scroll(itemTarget);
+  };
+});
+
+window.addEventListener(`scroll`, (e) => {
+  scrollEvent = e;
+});
